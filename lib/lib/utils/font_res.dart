c3619d019950b63ef0adee06af10b2d5936563f1
mobileappdev47 <88827091+mobileappdev47@users.noreply.github.com>
2023-07-19 17:29:27 +0530

    new change

--- lib/lib/utils/font_res.dart
+++
@@ -1,6 +0,0 @@
-class FontRes{
-  static const poppinsBold= "Poppins-Thin";
-  static const poppinsRegular= "Poppins-Regular";
-  static const poppinsMedium= "Poppins-Medium";
-  static const poppinsSemiBold= "Poppins-SemiBold";
-}
