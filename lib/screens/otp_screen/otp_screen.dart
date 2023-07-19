5478efaadefaafc6362e85e075ba9d15c4fb15d0
mobileappdev47 <88827091+mobileappdev47@users.noreply.github.com>
2023-07-19 17:30:57 +0530

    c

--- lib/screens/otp_screen/otp_screen.dart
+++ lib/screens/otp_screen/otp_screen.dart
@@ -43,85 +43,88 @@ class OtpScreen extends StatelessWidget {
                       child: Padding(
                         padding:
                             EdgeInsets.symmetric(horizontal: Get.width * 0.06),
-                        child: Column(
-                          children: [
-                            SizedBox(
-                              height: Get.height * 0.02,
-                            ),
-                            Row(
-                              children: [
-                                InkWell(
-                                  splashFactory: NoSplash.splashFactory,
-                                  onTap: () {
-                                    Get.back();
-                                  },
-                                  child: Image.asset(
-                                    AssetRes.arrowLeft,
-                                    width: 26,
-                                  ),
-                                ),
-                                const Spacer(),
-                              ],
-                            ),
-                            SizedBox(
-                              height: Get.height * 0.05,
-                            ),
-                            const Text(
-                              StringRes.enterCode,
-                              style: TextStyle(
-                                  fontSize: 30,
-                                  fontFamily: FontRes.poppinsRegular,
-                                  fontWeight: FontWeight.w600),
-                            ),
-                            SizedBox(
-                              height: Get.height * 0.07,
-                            ),
-                            VerificationCode(
-                                onCompleted: (value) {},
-                                length: 4,
-                                margin: EdgeInsets.symmetric(
-                                    horizontal: Get.width * 0.02),
-                                onEditing: (value) {}),
-                            
-                            SizedBox(height: Get.height * 0.04,),
-                            Text(StringRes.resend,style: TextStyle(
-                                fontSize: 15,
-                                // decoration: TextDecoration.underline,
-                                shadows: [Shadow(
-                                  color: Colors.grey.shade200,blurRadius: 5
-                                )],
-                                fontFamily: FontRes.poppinsRegular,
-                                fontWeight: FontWeight.w500,
-                                color: ColorRes.color8E8E8E
-                            ),),
-                            SizedBox(
-                              height: Get.height * 0.15,
-                            ),
-                            Card(
-                              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
-                              elevation: 5,
-                              child: GestureDetector(
-                                onTap:(){
-                                  Get.to(()=> OtpScreen());
-                                },
-                                child: Container(
-                                  height:  45,
-                                  width: 300,
-                                  alignment: Alignment.center,
-                                  decoration: BoxDecoration(
-                                      color: ColorRes.commonButtonColor,
-                                      borderRadius: BorderRadius.circular(30)
-                                  ),
-                                  child:  Text(StringRes.verify.toUpperCase(),style:
-                                  const TextStyle(
-                                      fontSize: 15,
-                                      fontFamily: FontRes.poppinsMedium,
-                                      fontWeight: FontWeight.w500,
-                                      color: ColorRes.white),),
-                                ),
+                        child: SingleChildScrollView(
+                          physics: NeverScrollableScrollPhysics(),
+                          child: Column(
+                            children: [
+                              SizedBox(
+                                height: Get.height * 0.02,
                               ),
-                            )
-                          ],
+                              Row(
+                                children: [
+                                  InkWell(
+                                    splashFactory: NoSplash.splashFactory,
+                                    onTap: () {
+                                      Get.back();
+                                    },
+                                    child: Image.asset(
+                                      AssetRes.arrowLeft,
+                                      width: 26,
+                                    ),
+                                  ),
+                                  const Spacer(),
+                                ],
+                              ),
+                              SizedBox(
+                                height: Get.height * 0.05,
+                              ),
+                              const Text(
+                                StringRes.enterCode,
+                                style: TextStyle(
+                                    fontSize: 30,
+                                    fontFamily: FontRes.poppinsRegular,
+                                    fontWeight: FontWeight.w600),
+                              ),
+                              SizedBox(
+                                height: Get.height * 0.07,
+                              ),
+                              VerificationCode(
+                                  onCompleted: (value) {},
+                                  length: 4,
+                                  margin: EdgeInsets.symmetric(
+                                      horizontal: Get.width * 0.02),
+                                  onEditing: (value) {}),
+
+                              SizedBox(height: Get.height * 0.04,),
+                              Text(StringRes.resend,style: TextStyle(
+                                  fontSize: 15,
+                                  // decoration: TextDecoration.underline,
+                                  shadows: [Shadow(
+                                    color: Colors.grey.shade200,blurRadius: 5
+                                  )],
+                                  fontFamily: FontRes.poppinsRegular,
+                                  fontWeight: FontWeight.w500,
+                                  color: ColorRes.color8E8E8E
+                              ),),
+                              SizedBox(
+                                height: Get.height * 0.15,
+                              ),
+                              Card(
+                                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+                                elevation: 5,
+                                child: GestureDetector(
+                                  onTap:(){
+                                    Get.to(()=> OtpScreen());
+                                  },
+                                  child: Container(
+                                    height:  45,
+                                    width: 300,
+                                    alignment: Alignment.center,
+                                    decoration: BoxDecoration(
+                                        color: ColorRes.commonButtonColor,
+                                        borderRadius: BorderRadius.circular(30)
+                                    ),
+                                    child:  Text(StringRes.verify.toUpperCase(),style:
+                                    const TextStyle(
+                                        fontSize: 15,
+                                        fontFamily: FontRes.poppinsMedium,
+                                        fontWeight: FontWeight.w500,
+                                        color: ColorRes.white),),
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
                         ),
                       ),
                     ),
