97b795a68786b325f3fb742db49bb5a05d9f8d38
mobileappdev47 <88827091+mobileappdev47@users.noreply.github.com>
2023-07-19 17:14:29 +0530

    change

--- lib/lib/utils/asset_res.dart
+++ lib/lib/utils/asset_res.dart
@@ -16,14 +16,14 @@ class AssetRes {
   // ================================================images==================================
 
 // static const blackLogo = "${images}logo_black.png";
-static const blackLogo = "${images}blackLogo.png";
-static const phoneBg = "${images}backPhone.png";
-static const first = "${images}13 Pro - 1.png";
-static const google = "${images}Google Logo.png";
-static const phone = "${images}Phone Logo.png";
-static const apple = "${images}Apple Logo.png";
-static const backGround = "${images}backGround.png";
-static const otpBg = "${images}otpBg.png";
+  static const blackLogo = "${images}blackLogo.png";
+  static const phoneBg = "${images}backPhone.png";
+  static const first = "${images}13 Pro - 1.png";
+  static const google = "${images}Google Logo.png";
+  static const phone = "${images}Phone Logo.png";
+  static const apple = "${images}Apple Logo.png";
+  static const backGround = "${images}backGround.png";
+  static const otpBg = "${images}otpBg.png";
 
 
 }
---
+++ lib/lib/utils/colour_res.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: constant_identifier_names
+
+import 'package:flutter/material.dart';
+
+class ColorRes{
+
+  static const white = Colors.white;
+
+  static const F2609E = Color(0xFFF2609E);
+  static const FFFFFF = Color(0xFFFFFFFF);
+  static const FF373737 = Color(0xFF373737);
+  static const color606060 = Color(0xFF606060);
+  static const color8C8C8C = Color(0xFF8C8C8C);
+  static const color8E8E8E = Color(0xFF8E8E8E);
+  static const commonButtonColor = Color(0xFFF2609E);
+
+}
